d81a57371539f2273cc45874760e880f5a8fe623
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-11-24 23:51:24 +0200

    My first neural network in pytorch

---
+++ 2ndFirstNN/NN.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn 
+
+
+
+
+input_tensor = torch.tensor([[0.3471,0.4547,-0.2356]])
+
+linear_layer = nn.Linear(in_features= 3 , out_features= 2)
+
+output = linear_layer(input_tensor)
+
+print(output)
