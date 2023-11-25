ce9e71ae991aca912f841bbcf06d098e1ebddda4
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-11-25 23:59:57 +0200

    updated sigmoid example to compare between sigmoid and non sigmoid result

--- 3rdActivations_sigmoid/SigmoidActivation.py
+++ 3rdActivations_sigmoid/SigmoidActivation.py
@@ -1,10 +1,13 @@
 import torch
 import torch.nn as nn
-
+import numpy as np
 from SyntheticDataGenerator import generate_synthetic_data
 
-X_tensor, Y_tensor = generate_synthetic_data(num_samples= 100, num_features=10)
+#X_tensor, Y_tensor = generate_synthetic_data(num_samples= 100, num_features=10,seed=100)
 
+# Generating a random input sample to test the models
+random_input_sample = torch.randint(100,1000,(1, 10))  # 1 sample with 10 features
+random_input_sample = random_input_sample.float()
 
 model = nn.Sequential(
     nn.Linear(10,5),
@@ -12,16 +15,13 @@ model = nn.Sequential(
     nn.Sigmoid()
 )
 
-output = model(X_tensor[0])
+output = model(random_input_sample)
 print("Model output for the first sample:", output.item())
 
+model_nosigmoid = nn.Sequential(
+    nn.Linear(10,5),
+    nn.Linear(5,1)
+)
 
-# Forward pass for the entire dataset
-outputs = model(X_tensor).squeeze()  # Remove unnecessary dimensions
-
-# Binarize outputs (using 0.5 as the threshold)
-predicted = (outputs > 0.5).float()
-
-# Calculate accuracy
-accuracy = (predicted == Y_tensor).float().mean()
-print("Accuracy on the synthetic dataset:", accuracy.item())
+output_nosigmoid = model_nosigmoid(random_input_sample)
+print("Model output for the first sample:", output_nosigmoid.item())
