02ace846920e68fc4eb4e6f244b4d95c3471239a
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-11-26 00:11:00 +0200

    Modified model for different outputs, fixed prints, added gitignore

--- 3rdActivations_sigmoid/SigmoidActivation.py
+++ 3rdActivations_sigmoid/SigmoidActivation.py
@@ -10,18 +10,22 @@ random_input_sample = torch.randint(100,1000,(1, 10))  # 1 sample with 10 featur
 random_input_sample = random_input_sample.float()
 
 model = nn.Sequential(
-    nn.Linear(10,5),
-    nn.Linear(5,1),
+    nn.Linear(10,20),
+    nn.Linear(20,40),
+    nn.Linear(40,10),
+    nn.Linear(10,1),
     nn.Sigmoid()
 )
 
 output = model(random_input_sample)
-print("Model output for the first sample:", output.item())
+print("Model output with the sigmoid:", output.item())
 
 model_nosigmoid = nn.Sequential(
-    nn.Linear(10,5),
-    nn.Linear(5,1)
+    nn.Linear(10,20),
+    nn.Linear(20,40),
+    nn.Linear(40,10),
+    nn.Linear(10,1)
 )
 
 output_nosigmoid = model_nosigmoid(random_input_sample)
-print("Model output for the first sample:", output_nosigmoid.item())
+print("Model output without the sigmoid:", output_nosigmoid.item())
