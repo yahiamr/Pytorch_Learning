efd78a58c3f5885d8e22cf83d0f3c7c2b46d9496
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-11-27 23:52:55 +0200

    Forward pass, created synth. multiclass data generator, and one hot encoding

--- 2ndFirstNN/NN.py
+++ 2ndFirstNN/NN.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn as nn 
 
-
-
-
 input_tensor = torch.tensor([[0.3471,0.4547,-0.2356]])
 
 linear_layer = nn.Linear(in_features= 3 , out_features= 2)
---
+++ 4thForwardPass_OHE/Classification_OneHotEncoding.py
@@ -0,0 +1,23 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from Synthetic_MultiClassData import generate_multiclass_synthetic_data
+
+X_data, y_data = generate_multiclass_synthetic_data(100,5,3)
+
+model = nn.Sequential(
+    nn.Linear(5,20),
+    nn.Linear(20,40),
+    nn.Linear(40,3),
+    nn.Softmax(dim=-1)
+)
+
+results = model(X_data)
+print(results[0:10])
+
+predicted_labels = torch.argmax(results,dim=1)
+print(predicted_labels[0:10])
+
+hot_encoded_data = F.one_hot(predicted_labels,num_classes=3)
+print(hot_encoded_data[0:10])
+
