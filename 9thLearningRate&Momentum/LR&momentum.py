f5818154f8433f04c077059cee49a6c5d41b3f7c
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-08 23:57:30 +0200

    NN arch. with more layers and how to get number of parameters, difference between learning rate and momentum for optimizers

--- 9thLearningRate&Momentum/LR&momentum.py
+++ 9thLearningRate&Momentum/LR&momentum.py
@@ -2,3 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+model = nn.Sequential(nn.Linear(10,20),
+                      nn.Linear(20,40),
+                      nn.Linear(40,5))
+
+optimizer = optim.SGD(model.parameters(), lr= 0.1, momentum=0.9)
