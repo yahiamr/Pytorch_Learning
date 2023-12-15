89f278722ba2e27d42709f785a76dbc03c3f1015
yahiamr <146677961+yahiamr@users.noreply.github.com>
2023-12-15 23:20:53 +0200

    Update CNN_MNIST_Lenet5.py with full arch

--- CNN/CNN_MNIST_Lenet5.py
+++ CNN/CNN_MNIST_Lenet5.py
@@ -1,4 +1,19 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
 
+# Step 2: Load MNIST Dataset
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+
+# Step 3: Define the LeNet-5 Model
 class LeNet5(nn.Module):
     def __init__(self):
         super(LeNet5, self).__init__()
@@ -26,3 +41,19 @@ class LeNet5(nn.Module):
 
 net = LeNet5()
 
+# Step 4: Define Loss Function and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+# Step 5: Train the Model in Batches
+for epoch in range(10):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print(f'Epoch {epoch + 1}, Loss: {running_loss / len(trainloader)}')
