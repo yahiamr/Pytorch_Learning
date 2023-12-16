79794d5712f8c0cdbcf3a429b622fa323bb7fecc
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-17 00:49:58 +0200

    added file using segment model

--- YOLO/yoloscript.py
+++ YOLO/yoloscript.py
@@ -1,5 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8m.pt')
+model = YOLO('yolov8s.pt')
 
-results = model(source=1, show= True, conf = 0.4)
+#results = model(source=1, show= True, conf = 0.6)
+
+seg_model = YOLO('yolov8n-seg.pt')
+
+results = seg_model(source=1, show= True, conf = 0.6)
+
