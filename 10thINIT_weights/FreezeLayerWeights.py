602bfd2aa638e198c8b006f8215ac78abeea5a93
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-24 00:33:13 +0200

    added a training step and check if parameters have changed

--- 10thINIT_weights/FreezeLayerWeights.py
+++ 10thINIT_weights/FreezeLayerWeights.py
@@ -12,3 +12,26 @@ for name , param in model.named_parameters():
 
 
 
+# Save the initial state of the parameters
+initial_params = {name: param.clone() for name, param in model.named_parameters()}
+
+# Dummy input and target (for demonstration purposes)
+input = torch.randn(1, 64)
+target = torch.randn(1, 256)
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.01)
+
+# Perform a training step
+optimizer.zero_grad()
+output = model(input)
+loss = criterion(output, target)
+loss.backward()
+optimizer.step()
+
+# Check if the parameters have changed
+for name, param in model.named_parameters():
+    if param.requires_grad:
+        changed = not torch.equal(initial_params[name], param)
+        print(f'Parameter {name} changed: {changed}')
+    else:
+        print(f'Parameter {name} is frozen.')
