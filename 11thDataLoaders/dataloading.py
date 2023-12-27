0454a782126a38a1c68db689e8f010a0fe84cbb0
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-27 23:59:20 +0200

    Created data loader and parameters for it

--- 11thDataLoaders/dataloading.py
+++ 11thDataLoaders/dataloading.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 import torch 
-from torch.utils.data import TensorDataset
+from torch.utils.data import TensorDataset , DataLoader
 df = pd.read_csv('./11thDataLoaders/animals.csv')
 
 
@@ -15,3 +15,17 @@ y = np.array(target)
 print(y)
 
 dataset = TensorDataset(torch.tensor(X).float(),torch.tensor(y).float())
+
+sample = dataset[0]
+X_sample, y_sample = sample
+print("X Sample",X_sample)
+print("y sample",y_sample)
+
+batch_size = 2
+shuffle = True
+
+dataloader = DataLoader(dataset, batch_size=batch_size,shuffle=shuffle)
+
+for batch_X, batch_y in dataloader:
+    print("X Batch", batch_X)
+    print("y batch", batch_y)
