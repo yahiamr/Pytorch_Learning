5622a0475756e027eb0c0bce9a6ec15749c060a2
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-30 23:48:49 +0200

    defined class model in train&evaluate script

--- 12thModelEvaluation/Train&Evaluate.py
+++ 12thModelEvaluation/Train&Evaluate.py
@@ -3,6 +3,19 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
+
+class YourModel(nn.Module):
+    def __init__(self):
+        super(YourModel, self).__init__()
+        # Define layers
+        pass
+
+    def forward(self, x):
+        # Define forward pass
+        pass
+
+
+##########
 def train_one_epoch(model,dataloader,optimizer,criterion):
     count = 0
     for data_batch in dataloader:
@@ -19,4 +32,3 @@ def train_one_epoch(model,dataloader,optimizer,criterion):
         loss.backward()
 
         optimizer.step()
-
