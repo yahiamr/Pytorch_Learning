dc4f419fa95deede033893334756b2118561b7b8
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-31 23:42:18 +0200

    Created GenericNet class to be used to instantiate our model later

---
+++ IRIS_test/GenericNet.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+class GenericNet(nn.Module):
+    def __init__(self, input_size, hidden_layers, output_size, activation_fn=nn.ReLU):
+        """
+        Generic neural network model.
+
+        :param input_size: Number of input features.
+        :param hidden_layers: List containing the number of units in each hidden layer.
+        :param output_size: Number of output units.
+        :param activation_fn: Activation function to be used in hidden layers.
+        """
+        super(GenericNet, self).__init__()
+        layers = []
+        last_size = input_size
+
+        # Create hidden layers
+        for hidden_size in hidden_layers:
+            layers.append(nn.Linear(last_size, hidden_size))
+            layers.append(activation_fn())
+            last_size = hidden_size
+
+        # Add output layer
+        layers.append(nn.Linear(last_size, output_size))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
--- IRIS_test/IRIS_.py
+++ IRIS_test/IRIS_.py
@@ -5,3 +5,17 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
+# Load the iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Convert to PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.long)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.long)
