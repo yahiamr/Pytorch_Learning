6b3c3819a8ff2ccaa5b6403405fddbd53c7ab81b
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-01-08 23:46:43 +0200

    printed the tensor after the dropout to see the effect

--- 13thFightingOverfitting/dropout_layer.py
+++ 13thFightingOverfitting/dropout_layer.py
@@ -1,8 +1,11 @@
 import torch.nn as nn
 import torch
 
-model = nn.Sequential(nn.Linear(8,4),
+model = nn.Sequential(nn.Linear(8,6),
                       nn.ReLU(),
                       nn.Dropout(p=0.5))
 
 features = torch.randn((1,8))
+
+output = model(features)
+print(output)
