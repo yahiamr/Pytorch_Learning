ad0081d9e133f813b895854ac81d120ecedc0e18
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-01-08 23:52:22 +0200

    added more than an iteration to test the randomness of the dropout

--- 13thFightingOverfitting/dropout_layer.py
+++ 13thFightingOverfitting/dropout_layer.py
@@ -9,3 +9,9 @@ features = torch.randn((1,8))
 
 output = model(features)
 print(output)
+
+# Pass the input through the model multiple times with different seeds
+for i in range(5):
+    torch.manual_seed(torch.initial_seed() + i)  # Change the seed
+    output = model(features)
+    print(f"Iteration {i+1}: {output}")
